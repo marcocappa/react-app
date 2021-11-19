6662b7498743ac39aaf83b5fa330f6799b9cd2dd
Marco Cappa <marcocappa85@gmail.com>
2021-11-19 16:35:12 +0000

    created a button component and some basic style with colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Button from './components/Button';
 
 const APIs_URL = {
   INSTALLED: 'http://localhost:5001/installed',
   UNINSTALLED: 'http://localhost:5001/uninstalled',
 };
 
-function App() {
+function App(): JSX.Element {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<null | string>(null);
   const [uninstalled, setUninstalled] = useState([]);
@@ -29,6 +30,13 @@ function App() {
   return (
     <div>
       <h1>SDK App</h1>
+      <Button
+        onClick={() => {
+          console.log('clicked');
+        }}
+      >
+        Click me
+      </Button>
       {!error && !loading && uninstalled.length > 0 && uninstalled.map(({ id, name }) => <li key={id}>{name}</li>)}
     </div>
   );
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import classnames from 'classnames';
+import './style.scss';
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+  title?: string;
+  isDisabled?: boolean;
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+function Button({ className, children, onClick, isDisabled }: Props): JSX.Element {
+  return (
+    <button
+      className={`react-app-button ${classnames(className)}`}
+      data-testid="test-button"
+      onClick={(e) => onClick(e)}
+      disabled={isDisabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
