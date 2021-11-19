7c2007107dae48b334d9fe9a495e493e5313b0b3
Marco Cappa <marcocappa85@gmail.com>
2021-11-19 19:06:02 +0000

    used get categories to create a state in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import { API_URL, RESOURCES } from './utils/constants';
+import { getCategories } from './utils';
 import './App.scss';
 
 function App(): JSX.Element {
@@ -9,14 +10,17 @@ function App(): JSX.Element {
   const [error, setError] = useState<null | string>(null);
   const [data, setData] = useState([]);
   const [resource, setResource] = useState(RESOURCES.INSTALLED);
+  const [categories, setCategories] = useState<string[]>([]);
 
   const fetchData = async (newResource: string) => {
     setLoading(true);
     try {
       const res = await fetch(`${API_URL}${newResource}`);
       const obj = await res.json();
-      setData(obj.data[`${newResource}Sdks`]);
+      const data = obj.data[`${newResource}Sdks`];
+      setData(data);
       setResource(newResource);
+      setCategories(getCategories(data));
     } catch (err) {
       setError('Ops! Something went wrong with the API!');
     }
@@ -38,7 +42,13 @@ function App(): JSX.Element {
       {loading && <p>Loading data...</p>}
       {!loading && error && <p>{error}</p>}
       {!loading && !error && data.length > 0 && (
-        <Card data={data} heading={`${resource} SDKs`} latestUpdate="30 july 2021" total={data.length} />
+        <Card
+          categories={categories}
+          data={data}
+          heading={`${resource} SDKs`}
+          latestUpdate="30 july 2021"
+          total={data.length}
+        />
       )}
     </div>
   );
